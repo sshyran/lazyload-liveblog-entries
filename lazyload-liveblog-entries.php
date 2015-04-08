031a8025144539be4866acd7a77b5c814cedcdcd
Daniel Bachhuber <d@danielbachhuber.com>
2015-04-08 13:03:29 -0700

    Scaffold plugin

---
+++ lazyload-liveblog-entries.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Lazyload-liveblog-entries
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: lazyload-liveblog-entries
+Domain Path: /languages
+*/
+
